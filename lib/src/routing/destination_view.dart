66d69ec0ea3d5df524d4ee1764d88009af11e2d5
Meg-Ann <meg-ann.camus@outlook.fr>
2023-12-10 23:02:09 +0100

    Routing is OK

---
+++ lib/src/routing/destination_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'destination.dart';
+
+class DestinationView extends StatefulWidget {
+  const DestinationView({
+    super.key,
+    required this.destination,
+    required this.navigatorKey,
+  });
+
+  final Destination destination;
+  final Key navigatorKey;
+
+  @override
+  State<DestinationView> createState() => _DestinationViewState();
+}
+
+class _DestinationViewState extends State<DestinationView> {
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: widget.navigatorKey,
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute<void>(
+          settings: settings,
+          builder: (BuildContext context) {
+            switch (settings.name) {
+              case '/':
+                return widget.destination.page;
+              //case '/list':
+              //  return ListPage(destination: widget.destination);
+            }
+            assert(false);
+            return const SizedBox();
+          },
+        );
+      },
+    );
+  }
+}
